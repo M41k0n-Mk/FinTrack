b2d8de8d70ea67baa98b221a528db8cc937df8b1
M41k0n <142348031+M41k0n-Mk@users.noreply.github.com>
2025-09-28 20:45:44 -0300

    refactor: update controller to use application service instead of calling use case directly

--- src/main/java/me/m41k0n/investment/presentation/controller/InvestmentController.java
+++ src/main/java/me/m41k0n/investment/presentation/controller/InvestmentController.java
@@ -2,12 +2,9 @@ package me.m41k0n.investment.presentation.controller;
 
 import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
-import me.m41k0n.investment.application.usecase.RegisterInvestmentUseCase;
-import me.m41k0n.investment.application.usecase.command.RegisterInvestmentCommand;
-import me.m41k0n.investment.domain.Investment;
+import me.m41k0n.investment.application.service.InvestmentApplicationService;
 import me.m41k0n.investment.presentation.dto.InvestmentRequest;
 import me.m41k0n.investment.presentation.dto.InvestmentResponse;
-import me.m41k0n.investment.presentation.mapper.InvestmentMapper;
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.*;
 
@@ -16,20 +13,11 @@ import org.springframework.web.bind.annotation.*;
 @RequiredArgsConstructor
 public class InvestmentController {
 
-    private final RegisterInvestmentUseCase registerInvestmentUseCase;
+    private final InvestmentApplicationService investmentApplicationService;
 
     @PostMapping
     @ResponseStatus(HttpStatus.CREATED)
     public InvestmentResponse register(@Valid @RequestBody InvestmentRequest request) {
-        RegisterInvestmentCommand command = new RegisterInvestmentCommand(
-                request.name(),
-                request.type(),
-                request.investmentValue(),
-                request.purchaseDate(),
-                request.broker(),
-                request.purchaseRate()
-        );
-        Investment investment = registerInvestmentUseCase.execute(command);
-        return InvestmentMapper.INSTANCE.toResponse(investment);
+        return investmentApplicationService.registerInvestment(request);
     }
 }
